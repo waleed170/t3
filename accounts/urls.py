b081d811c6c55bdfb833a606c9c6f88c59df6f17
Muhammad Waleed Monim <waleedmonim18@gmail.com>
2025-06-17 16:20:50 +0500

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+# accounts/urls.py
+from django.urls import path
+from .views import register, login_view
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    
+]
---
+++ accounts/views.py
@@ -0,0 +1,31 @@
+# accounts/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, 'Registration successful. You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'accounts/login.html')
+
